ce64d87dc75ccc9da604e6ce7ad17de060870790
mfaseeh500 <93811900+mfaseeh500@users.noreply.github.com>
2023-03-30 00:16:48 +0500

    30/3/2023 (1st commit)

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ educationBtn.addEventListener('click', function() {
   document.getElementById('education').classList.add("hidden");
   document.getElementById('experience').classList.add("hidden"); 
   document.getElementById('projects').classList.add("hidden");
- 
 });
 //when click on education button
     var educationBtn = document.getElementById('education-btn');
@@ -13,20 +12,16 @@ educationBtn.addEventListener('click', function() {
       document.getElementById('introduction').classList.add("hidden"); 
       document.getElementById('education').classList.remove("hidden");
       document.getElementById('experience').classList.add("hidden"); 
-      document.getElementById('projects').classList.add("hidden");
-     
+      document.getElementById('projects').classList.add("hidden");  
     });
-
 //when click on work experience button
 var workExperienceBtn = document.getElementById('workexperience-btn');
 workExperienceBtn.addEventListener('click', function() {
   document.getElementById('introduction').classList.add("hidden"); 
   document.getElementById('education').classList.add("hidden");
   document.getElementById('experience').classList.remove("hidden"); 
-  document.getElementById('projects').classList.add("hidden");
- 
+  document.getElementById('projects').classList.add("hidden"); 
 });
-
 //when click on projects button
 var projectsBtn = document.getElementById('projects-btn');
 projectsBtn.addEventListener('click', function() {
@@ -34,5 +29,4 @@ projectsBtn.addEventListener('click', function() {
   document.getElementById('education').classList.add("hidden");
   document.getElementById('experience').classList.add("hidden"); 
   document.getElementById('projects').classList.remove("hidden");
- 
 });
